79e560697a8df31ed167bf243c1755f736f29fd7
codeswithrohit <hrohit320@gmail.com>
2024-03-24 13:11:34 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,9 @@
-import Link from "next/link";
-import React, { useState } from "react";
-import dynamic from "next/dynamic";
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import HomePage from "../src/components/HomePage";
-import PGHOME from "../src/components/Home/PGHOME";
-import BUYHOME from "../src/components/Home/BUYHOME";
-import RENT from "../src/components/Home/RENT";
-import HOTEL from "../src/components/Home/HOTEL";
-const Counter = dynamic(() => import("../src/components/Counter"), {
-  ssr: false,
-});
-
+import React from 'react'
 
 const Index = () => {
-
   return (
-    <div>
-   
-      {/* <!--====== Start Hero Section ======--> */}
-    <HomePage/>
-      {/* <!--====== Start PG Listing Section ======--> */}
-   <PGHOME/>
-   {/* <!--====== Start BUY Property Listing Section ======--> */}
-  <BUYHOME/>
-  {/* <!--====== Start RENT Property Listing Section ======--> */}
-  <RENT/>
-  {/* <!--====== Start HOTEL Property Listing Section ======--> */}
-  <HOTEL/>
-    </div>
-  );
-};
-export default Index;
+    <div>Index</div>
+  )
+}
+
+export default Index
